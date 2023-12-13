7408a0c2dd7a7af42cc4cc432b95ef157651886f
Dan <bogdanlinee@gmail.com>
2023-12-13 07:56:16 +0200

    deleted dist derectory

--- dist/src/db/seeds/user.seeder.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-class UserSeeder {
-    async run(dataSource, factoryManager) {
-        console.log('hello world');
-    }
-}
-exports.default = UserSeeder;
-//# sourceMappingURL=user.seeder.js.map
