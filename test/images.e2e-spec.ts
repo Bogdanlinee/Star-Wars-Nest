a5fa20bfd2963c30453d17daf4284dc3ca6f6a36
Dan <bogdanlinee@gmail.com>
2024-01-05 16:06:02 +0200

    Finished tests for Images module

--- test/images.e2e-spec.ts
+++ test/images.e2e-spec.ts
@@ -19,10 +19,36 @@ describe('AppController (e2e)', () => {
     it('Can add one image', async () => {
         return request(app.getHttpServer())
             .post(`/image/add/1`)
-            .attach('file', 'uploads/1.jpg')
+            .attach('file', './test/testImageFolder/1.jpg')
             .expect(201)
             .then(res => {
                 deleteFileCloudinary(res.body.publicId);
+            })
+    });
+
+    it('Throws Error. Add one image', async () => {
+        return request(app.getHttpServer())
+            .post(`/image/add/1`)
+            .expect(400)
+    });
+
+    it('Can delete one image', async () => {
+        return request(app.getHttpServer())
+            .post(`/image/add/1`)
+            .attach('file', './test/testImageFolder/1.jpg')
+            .expect(201)
+            .then(res => {
+                const image = res.body.image;
+                const deleteImageUrl = res.body.publicId;
+                return request(app.getHttpServer())
+                    .delete('/image/delete/1')
+                    .send({image})
+                    .expect(200)
+                    .then(res => {
+                        const {deletedAt} = res.body;
+                        expect(deletedAt).toBeTruthy();
+                        deleteFileCloudinary(deleteImageUrl);
+                    })
             })
     });
 });
