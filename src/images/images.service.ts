5895f3198043aa5fced255afb82d0ac4303e2941
Dan <bogdanlinee@gmail.com>
2024-02-07 21:08:40 +0200

    Testint aws s3

--- src/images/images.service.ts
+++ src/images/images.service.ts
@@ -7,40 +7,65 @@ import {uploadFileCloudinary, deleteFileCloudinary} from '../utils/cloudinaryFil
 import {UploadApiResponse} from 'cloudinary';
 import {unlink} from 'fs/promises';
 import {Person} from '../people/entities/person.entity';
+import {ConfigService} from '@nestjs/config';
+import * as process from 'process';
+import {PutObjectCommand, S3Client} from '@aws-sdk/client-s3';
+import * as fs from 'fs';
 
 @Injectable()
 export class ImagesService {
+    private readonly s3Client = new S3Client([{
+        region: process.env.AWS_S3_REGION,
+        accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+        secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
+    }]);
+
     constructor(
         @InjectRepository(ImagePerson)
         private imagePersonRepository: Repository<ImagePerson>,
         @InjectRepository(Person)
-        private peopleRepository: Repository<Person>
+        private peopleRepository: Repository<Person>,
     ) {
     }
 
     async addImage(file: Express.Multer.File, id: number) {
-        const person = await this.peopleRepository.findOne({where: {id}});
         const filePath = path.join(process.cwd(), file.path);
+        console.log(file);
 
-        if (!person) {
-            await unlink(filePath);
-            throw new NotFoundException('No such user!')
-        }
+        const buffer = fs.readFileSync(filePath);
 
-        const fileName = file.filename;
-        const uploadedFile = await uploadFileCloudinary(filePath, fileName) as UploadApiResponse | null;
+        await this.s3Client.send(new PutObjectCommand({
+                Bucket: 'wbb-starwars',
+                Key: file.filename,
+                Body: buffer,
+            })
+        )
 
-        await unlink(filePath);
+        return;
 
-        if (!uploadedFile) throw new BadRequestException('Something went wrong, please upload file again!');
 
-        const {url: imageUrl, public_id: imagePublicId} = uploadedFile;
-
-        const image = this.imagePersonRepository.create({image: imageUrl, publicId: imagePublicId});
-
-        image.person = person;
-
-        return await this.imagePersonRepository.save(image);
+        // const person = await this.peopleRepository.findOne({where: {id}});
+        // const filePath = path.join(process.cwd(), file.path);
+        //
+        // if (!person) {
+        //     await unlink(filePath);
+        //     throw new NotFoundException('No such user!')
+        // }
+        //
+        // const fileName = file.filename;
+        // const uploadedFile = await uploadFileCloudinary(filePath, fileName) as UploadApiResponse | null;
+        //
+        // await unlink(filePath);
+        //
+        // if (!uploadedFile) throw new BadRequestException('Something went wrong, please upload file again!');
+        //
+        // const {url: imageUrl, public_id: imagePublicId} = uploadedFile;
+        //
+        // const image = this.imagePersonRepository.create({image: imageUrl, publicId: imagePublicId});
+        //
+        // image.person = person;
+        //
+        // return await this.imagePersonRepository.save(image);
     }
 
     async deleteImage(id: number, image: string) {
--- src/utils/appendRelationEntities.ts
+++ src/utils/appendRelationEntities.ts
@@ -1,6 +1,6 @@
 export const appendEntities = async (entity: any, dto: any, idList: any, entityList: any, repository: any) => {
     if (dto[idList]) {
-        entity[entityList] = []
+        entity[entityList] = [];
         for (const id of dto[idList]) {
             const result = await repository.findOne({where: {id}});
             if (result) entity[entityList].push(result)
